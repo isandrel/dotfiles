e9ccbd140945501219c378b06b542adef13a8899
isandrel <isandrel.email@gmail.com>
2025-12-19 19:03:36 -0800

    Add .config/sketchybar/sketchybarrc Add .config/sketchybar/items/battery.sh Add .config/sketchybar/items/clock.sh Add .config/sketchybar/items/front_app.sh Add .config/sketchybar/items/spaces.sh Add .config/sketchybar/items/volume.sh Add .config/sketchybar/plugins/battery.sh Add .config/sketchybar/plugins/clock.sh Add .config/sketchybar/plugins/front_app.sh Add .config/sketchybar/plugins/icon_map.sh Add .config/sketchybar/plugins/space.sh Add .config/sketchybar/plugins/volume.sh

---
+++ dot_config/sketchybar/items/battery.sh
@@ -0,0 +1,8 @@
+# sketchybar \
+#     --add item battery right \
+#     --set battery update_freq=120 script="$PLUGIN_DIR/battery.sh" \
+#     --subscribe battery system_woke power_source_change
+
+sketchybar \
+    --add alias "AlDente,Item-0" right \
+    --set "AlDente,Item-0" update_freq=2
