735981ab40388f92bd87b11782f2fcbc3838aa47
isandrel <isandrel.email@gmail.com>
2026-04-06 20:55:59 -0700

    Update .DS_Store Update .config/nvim/README.md Update .config/nvim/.DS_Store Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/plugins/colorscheme.lua Update .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+        },
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
