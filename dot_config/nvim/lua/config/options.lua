df6ee171b1475622b24d0a5ebe7b94882619251c
isandrel <isandrel.email@gmail.com>
2026-04-11 00:04:29 -0700

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,4 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.wrap = true
+local cfg = require("lib.cfg")
+
+-- Apply all [vim] options from TOML config
+for k, v in pairs(cfg.get("vim", {})) do
+	vim.opt[k] = v
+end
