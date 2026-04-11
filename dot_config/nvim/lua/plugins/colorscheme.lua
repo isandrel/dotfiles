1413e13979e17ac74a33f58b899a44683a427397
isandrel <isandrel.email@gmail.com>
2026-04-11 00:04:30 -0700

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,26 @@
+-- Colorscheme plugin — configured via TOML
+local cfg = require("lib.cfg")
+
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-        },
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-      },
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	-- Catppuccin colorscheme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = cfg.get("ui.colorscheme_priority", 1000),
+		opts = {
+			flavour = cfg.get("ui.catppuccin_flavour", "mocha"),
+		},
+	},
+
+	-- Disable default tokyonight (shipped by LazyVim)
+	{ "folke/tokyonight.nvim", enabled = false },
+
+	-- Set colorscheme via LazyVim
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = cfg.get("ui.colorscheme", { "catppuccin-mocha", "habamax" })[1],
+		},
+	},
 }
